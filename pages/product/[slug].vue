9a0d4cf1ed35213e3bcd6548f8cac38a8da078a5
sven <sfolkerts89@gmail.com>
2024-03-25 14:35:12 +0100

    added back: pages/ boilerplate__extras pages

---
+++ pages/product/[slug].vue
@@ -0,0 +1,34 @@
+<template>
+	<main :key="setLocale">
+		<div class="singular-product">
+			<h1>{{ product.title }}</h1>
+			<blockquote>{{ product.description }}</blockquote>
+			<img :src="assetUrlBase + product.image" />
+			<PagesMarkdownRenderMarkdown :markdownString="product.content" />
+		</div>
+	</main>
+</template>
+<script setup>
+
+const route = useRoute();
+
+const { data: product } = reactive(await useAsyncData("product", () =>
+	queryContent("/products", route.params.slug).findOne())
+);
+
+const rtc = useRuntimeConfig();
+const cEnv = rtc.public.cloudinaryEnvUrl;
+const assetUrlBase = cEnv + "/image/upload" + '/c_scale/w_600/q_auto:best'
+</script>
+
+
+<style lang="scss">
+.singular-product {
+	max-width: 45em;
+	margin-inline: auto;
+	img {
+		position: relative;
+		width: 100%;
+	}
+}
+</style>
