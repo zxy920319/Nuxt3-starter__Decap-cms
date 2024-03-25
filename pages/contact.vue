479057b231ae519ab7f9e1ab5334e028f3f4712e
sven <sfolkerts89@gmail.com>
2024-03-25 14:35:12 +0100

    added back: pages/ boilerplate__extras pages

---
+++ pages/contact.vue
@@ -0,0 +1,31 @@
+<template>
+	<main>
+		<h1>contact</h1>
+		<FormsBasicInquery />
+	</main>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+	main {
+		background-position: center top;
+		background-size: 100%;
+		display: grid;
+		justify-items: center;
+		align-items: center;
+		@include media(xsm) {
+			display: block;
+		}
+		@include media(sm) {
+			@media screen and (orientation: portrait) {
+				display: block;
+			}
+		}
+	}
+	#basic-inquery {
+		max-width: 35em;
+		padding: $spacing4;
+		border: 3px solid black;
+	}
+</style>
---
+++ pages/dynamic-fields.vue
@@ -0,0 +1,17 @@
+<template>
+	<main id="main" class="dynamic-fields-showcase">
+		<PagesDynamicFields v-for="(zone, i) in dynamicFields.sections" :zone="zone"
+            :sid="'section-' + i" :key="i" />
+	</main>
+</template>
+
+<script setup>
+
+const { data: dynamicFields } = reactive(await useAsyncData("dynamic-fields", () =>
+	queryContent("/pages/dynamic-fields").findOne())
+);
+
+setSeoHead(dynamicFields.SEOmetaData);
+</script>
+
+<style lang="scss" scoped></style>
