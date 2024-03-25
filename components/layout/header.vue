9e3ee73575711c97d7f60b46718cc9e18b8d938e
sven <sfolkerts89@gmail.com>
2024-03-25 16:09:32 +0100

    added: bottom gradient to <header>

--- components/layout/header.vue
+++ components/layout/header.vue
@@ -8,12 +8,23 @@
 
 <style lang="scss" scoped>
 #header {
+	position: relative;
 	display: grid;
 	grid-template-columns: 1fr auto 1fr;
 	grid-template-rows: 1fr;
+	padding: $spacing2;
+	&::before {
+		position: absolute;
+		content: "";
+		width: 100%;
+		height: 0.1em;
+		top: 100%;
+		background-image: linear-gradient(90deg, $base-color, $secondary-color);
+	}
 
 	@include media(xsm) {
 		display: block;
+		padding: $spacing1 0;
 
 		#locale-toggle {
 			display: none;
