e4d11c5ce315589d13c636ce5b54b14ac6161cf3
sven <sfolkerts89@gmail.com>
2024-04-23 21:42:56 +0200

    got something working, server side? lets test

--- components/misc/ParseMarkdown.vue
+++ components/misc/ParseMarkdown.vue
@@ -23,20 +23,49 @@ watchEffect(async () => {
 
 
 <script setup>
+
 const props = defineProps({
-  markdownString: {
-    type: String,
-    required: true,
-  }
+	markdownString: {
+		type: String,
+		required: true,
+	}
 });
 
-const r = mdToHtml(props.markdownString);
+// const newHtml = await useAsyncData('new-html', () => mdToHtml(props.markdownString))
 
-// console.log(await r)
+const newHtml = await mdToHtml(props.markdownString)
 
+// console.log(newHtml)
+// const test = computed(() => {
+// 	return newHtml.data.innerHTML
+// })
+
+// const test = ref(newHtml.data.value)
+
+// console.log('test: ', test.value)
+
+onMounted(() => {
+	// function sethtml() {
+	let div = document.getElementById('me-here')
+	div.innerHTML += "test"
+	console.log(newHtml)
+
+	for (let i = 0; i < newHtml.length; i++) {
+		// create element from tag type
+		const element = document.createElement(newHtml[i].tag);
+		// create text node with children[0].value 
+		element.innerHTML += (newHtml[i].value);
+		// test element attributes
+		// console.log(element)
+		div.appendChild(element)
+	}
+	// }
+	// div.innerHTML += newHtml.data 
+	console.log('hello?', div)
+	// }
+})
 </script>
 
 <template>
-  <!-- <pre>?: {{  r }}</pre> -->
-  <!-- <div v-html="r" /> -->
+	<div id="me-here"></div>
 </template>
--- composables/mdToHtml.js
+++ composables/mdToHtml.js
@@ -1,37 +1,43 @@
 import { parseMarkdown } from '@nuxtjs/mdc/runtime'
 
-export default async function(md) {
-
-    const rawMd = await useAsyncData('markdown', () =>  parseMarkdown(md))
+export default async function (md) {
+    const rawMd = await useAsyncData('markdown', () => parseMarkdown(md))
     // Iterate to get the array of elements returned by parseMarkdown
     const result = await rawMd.data.value.body.children
     // Construct a new array
     const htmlObjects = new Array
     for (let i = 0; i < result.length; i++) {
-        // Testing
-        // console.log(result[i])
-        htmlObjects.push(result[i])
-            // Test htmlObjects data structure:
-            //  type: "", tag: ""
-            //  children[
-            //      type: = USELESS
-            //      value: = what we need along with Parent.tag    
-            // ]
+        const el = new Object({
+            tag: result[i].tag,
+            value: result[i].children[0].value
+        })
+        // htmlObjects.push(result[i])
+        htmlObjects.push(el)
     }
-    // create new wrapper element
-    const content = document.createElement("div");
-    // add a class
-    content.classList.add("markdown-to-html-content");
-    // add new elements to content
-    for(let i = 0; i < htmlObjects.length; i++) {
-        // create element from tag type
-        const element = document.createElement(htmlObjects[i].tag);
-        // create text node with children[0].value 
-        element.innerHTML += (htmlObjects[i].children[0].value).toString();
-        // test element attributes
-        // console.log(element)
-        content.appendChild(element)
-    }
-    // console.log(content)
-    return content
+
+    // async function createHtml() {
+    //     // create new wrapper element
+    //     const content = document.createElement("div");
+    //     // add a class
+    //     content.classList.add("markdown-to-html-content");
+    //     // add new elements to content
+
+    //     function doLoop() {
+    //         for (let i = 0; i < htmlObjects.length; i++) {
+    //             // create element from tag type
+    //             const element = document.createElement(htmlObjects[i].tag);
+    //             // create text node with children[0].value 
+    //             element.innerHTML += (htmlObjects[i].children[0].value).toString();
+    //             // test element attributes
+    //             // console.log(element)
+    //             content.appendChild(element)
+    //         }
+    //     }
+    //     doLoop();
+    //     console.log('2: ', 'loop = done?', content)
+    //     return content
+    // }
+    // const res = createHtml();
+    return htmlObjects
+    // console.log(res)
 }
