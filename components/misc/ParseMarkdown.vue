9642cd5ccc836cd8f2eebd499a112aebc5e5a7a5
sven <sfolkerts89@gmail.com>
2024-04-23 22:20:25 +0200

    testing simple v-html(String) rendering + including MDC script

---
+++ components/misc/ParseMarkdown.vue
@@ -0,0 +1,50 @@
+<script setup>
+
+const props = defineProps({
+	markdownString: {
+		type: String,
+		required: true,
+	}
+});
+
+// const newHtml = await useAsyncData('new-html', () => mdToHtml(props.markdownString))
+
+// const newHtml = await mdToHtml(props.markdownString)
+
+// console.log(newHtml)
+// const test = computed(() => {
+// 	return newHtml.data.innerHTML
+// })
+
+// const test = ref(newHtml.data.value)
+
+// console.log('test: ', test.value)
+
+// onMounted(() => {
+// 	// function sethtml() {
+// 	let div = document.getElementById('me-here')
+// 	div.innerHTML += "test"
+// 	console.log(newHtml)
+
+// 	for (let i = 0; i < newHtml.length; i++) {
+// 		// create element from tag type
+// 		const element = document.createElement(newHtml[i].tag);
+// 		// create text node with children[0].value 
+// 		element.innerHTML += (newHtml[i].value);
+// 		// test element attributes
+// 		// console.log(element)
+// 		div.appendChild(element)
+// 	}
+// 	// }
+// 	// div.innerHTML += newHtml.data 
+// 	console.log('hello?', div)
+// 	// }
+// })
+const test = await mdToHtml();
+
+console.log(test)
+</script>
+
+<template>
+	<div id="me-here" v-html="test"></div>
+</template>
--- composables/mdToHtml.js
+++ composables/mdToHtml.js
@@ -1,19 +1,21 @@
 import { parseMarkdown } from '@nuxtjs/mdc/runtime'
 
 export default async function (md) {
-    const rawMd = await useAsyncData('markdown', () => parseMarkdown(md))
-    // Iterate to get the array of elements returned by parseMarkdown
-    const result = await rawMd.data.value.body.children
-    // Construct a new array
-    const htmlObjects = new Array
-    for (let i = 0; i < result.length; i++) {
-        const el = new Object({
-            tag: result[i].tag,
-            value: result[i].children[0].value
-        })
-        // htmlObjects.push(result[i])
-        htmlObjects.push(el)
-    }
+    // const rawMd = await useAsyncData('markdown', () => parseMarkdown(md))
+    // // Iterate to get the array of elements returned by parseMarkdown
+    // const result = await rawMd.data.value.body.children
+    // // Construct a new array
+    // const htmlObjects = new Array
+    // for (let i = 0; i < result.length; i++) {
+    //     const el = new Object({
+    //         tag: result[i].tag,
+    //         value: result[i].children[0].value
+    //     })
+    //     // htmlObjects.push(result[i])
+    //     htmlObjects.push(el)
+    // }
+    // TODO:
+    // Create a string that resembles a HTMLobject
 
     // async function createHtml() {
     //     // create new wrapper element
@@ -38,6 +40,6 @@ export default async function (md) {
     //     return content
     // }
     // const res = createHtml();
-    return htmlObjects
+    return "<p>i am a paragraph</p><h2>i am a h2 heading</h2>"
     // console.log(res)
 }
