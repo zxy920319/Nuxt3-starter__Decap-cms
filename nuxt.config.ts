79376617ca1f361c1fe86dbe32ad5256e2eb2979
sven <sfolkerts89@gmail.com>
2024-04-29 09:04:07 +0200

    deleted noindex, nofollow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,6 @@ export default defineNuxtConfig({
 			charset: "utf-8",
 			viewport: "width=device-width, initial-scale=1",
 			title: "Nuxt & Decap CMS boilerplate",
-			meta: [{ name: "Sailsnake__nuxt-starter--decap-cms", content: "noindex, nofollow" }],
 		},
 	},
 	modules: ["@nuxt/content"],
