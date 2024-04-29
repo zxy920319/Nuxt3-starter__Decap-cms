b0aa4ddeaed5d1a210c8369811349c3c1556e162
sven <sfolkerts89@gmail.com>
2024-04-29 14:30:49 +0200

    fixed issue with mdToHtml not rendering single elements

--- composables/mdToHtml.js
+++ composables/mdToHtml.js
@@ -7,53 +7,64 @@ export default async function (md) {
     // htmlString will be populated with recursive functions and returned in the end
     let htmlString = ""
 
+    // THE FUNCTIONS in order from parent > child > value (Object > key, value)
+    
+    //-- 1.1
+    function createParentElement(elB) {
+        let elInner = new String
+        elInner = createChildElement(elB.value)
+        // console.log("// Return from 1.1: ", elB.tag, elInner)
+        return toElementString(elB.tag, elInner);
+    }
+
+    //-- 1.2
+    function createChildElement(elB) {
+        let childElementAsString = new String
+        for (let i = 0; i < elB.length; i++) {
+            if (elB[i].type === "text") {
+                childElementAsString += elB[i].value
+            }
+            else {
+                let elInner = new String
+                elInner = createChildElement(elB[i].children);
+                childElementAsString += toElementString(elB[i].tag, elInner);
+            }
+        }
+        // console.log("// return from 1.2: ", childElementAsString)
+        return childElementAsString
+    }
+
+    //-- 1.3 Returns <tag>value</tag> (or a linked version if tag === "a")
+    function toElementString(tag, value) {
+        let tagStart = new String
+        if (tag === "a") {
+            tagStart = "<" + `${tag}` + ' href="' + `${value}` + '"' + ' target="_blank"' + ">"
+        } else {
+            tagStart = "<" + `${tag}` + ">"
+        }
+        const tagEnd = "</" + `${tag}` + ">"
+        const res = tagStart + value + tagEnd
+        // console.log("// return from 1.3: ", res)
+        return res
+    }
+
+    
+    // 2.1
     function markdownToHtmlLoop(elA) {
         // Iterate through all the parent level array (A) returned from result
         for (let i = 0; i < elA.length; i++) {
             const elAO = new Object({
                 tag: elA[i].tag,
                 value: elA[i].children
+
             })
-            function createParentElement(elB) {
-                let elInner = new String
-
-                // Returns <tag>value</tag> (or a linked version if tag === "a")
-                // function called later down the line
-                function toElementString(tag, value) {
-                    let tagStart = new String
-                    if(tag === "a") {
-                        tagStart = "<" + `${tag}` + ' href="' + `${value}` + '"' + ' target="_blank"' + ">"
-                    } else {
-                        tagStart = "<" + `${tag}` + ">"
-                    }
-                    const tagEnd = "</" + `${tag}` + ">"
-                    const res = tagStart + value + tagEnd
-                    return res
-                }
-
-                if (elB.value.length === 1) {
-                    elInner = elB.value[0].value
-                } else {
-                    function createChildElement(elB) {
-                        let childElementAsString = new String
-                        for (let i = 0; i < elB.length; i++) {
-                            if (elB[i].type === "text") {
-                                childElementAsString += elB[i].value
-                            }
-                            else {
-                                elInner = createChildElement(elB[i].children);
-                                childElementAsString += toElementString(elB[i].tag, elInner);
-                            }
-                        }
-                        return childElementAsString
-                    }
-                    elInner = createChildElement(elB.value)
-                }
-                return toElementString(elB.tag, elInner);
-            }
+            // console.log("elAO (raw single parent Object from HTML array): ", elAO)
+            // console.log("!! return from main loop 2.1: ", createParentElement(elAO))
             htmlString += createParentElement(elAO)
         }
     }
+
+    // Call main loop (2.1)
     markdownToHtmlLoop(htmlObjectFromrawMd)
     // Return the entire HTML object as a single string; mount as you wish
     return htmlString
