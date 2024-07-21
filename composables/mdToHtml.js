d84f2821bf83bbbc3bf29592b258fb5aa2afd2b8
sven <sfolkerts89@gmail.com>
2024-07-21 21:59:38 +0200

    mdToHtml.js: added image rendering that checks absolute or relative path and uses runtimeConfig.public for relative url base

--- composables/mdToHtml.js
+++ composables/mdToHtml.js
@@ -1,14 +1,18 @@
 import { parseMarkdown } from '@nuxtjs/mdc/runtime'
 
+
 export default async function (md) {
+    const cEnv = useRuntimeConfig().public.cloudinaryEnvUrl + '/'
+    // console.log("yes?", cEnv)
     const rawMd = await useAsyncData('markdown', () => parseMarkdown(md))
     // // Iterate to get the array of elAements returned by parseMarkdown
     const htmlObjectFromrawMd = await rawMd.data.value.body.children
     // htmlString will be populated with recursive functions and returned in the end
     let htmlString = ""
 
+    console.log("htmlObjectFromrawMd", htmlObjectFromrawMd)
     // THE FUNCTIONS in order from parent > child > value (Object > key, value)
-    
+
     //-- 1.1
     function createParentElement(elB) {
         let elInner = new String
@@ -24,13 +28,20 @@ export default async function (md) {
             if (elB[i].type === "text") {
                 childElementAsString += elB[i].value
             }
+            else if (elB[i].tag == 'img') {
+                let src = elB[i].props.src
+                let className = 'markdown-image'
+                let isAbsolute = /^([a-z0-9]*:|.{0})\/\/.*$/.test(src)
+                childElementAsString = `<img src="${isAbsolute ? src : cEnv + src}" alt="${elB[i].props.alt}" class="${className}" />`
+
+            }
             else {
                 let elInner = new String
                 elInner = createChildElement(elB[i].children);
                 childElementAsString += toElementString(elB[i].tag, elInner);
             }
         }
-        // console.log("// return from 1.2: ", childElementAsString)
+        console.log("// return from 1.2: ", childElementAsString)
         return childElementAsString
     }
 
@@ -39,10 +50,10 @@ export default async function (md) {
         let tagStart = new String
         if (tag === "a") {
             tagStart = "<" + `${tag}` + ' href="' + `${value}` + '"' + ' target="_blank"' + ">"
-        } if(tag === "br") {
-            // console.log("yes")
+        } if (tag === "br") {
+            // console.log("1.3: br")
             // do nothing: stop tagStart from rendering a 'second' </br>
-        } 
+        }
         else {
             tagStart = "<" + `${tag}` + ">"
         }
@@ -52,7 +63,7 @@ export default async function (md) {
         return res
     }
 
-    
+
     // 2.1
     function markdownToHtmlLoop(elA) {
         // Iterate through all the parent level array (A) returned from result
