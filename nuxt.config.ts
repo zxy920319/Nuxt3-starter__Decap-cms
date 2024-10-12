94eb31f98415c6bc3c443f75265d887a3016d58e
zxy920319 <zxy920319@hotmail.com>
2024-10-12 14:40:33 +0100

    update packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,47 +1,56 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	app: {
-		head: {
-			htmlAttrs: {
-				lang: "en",
-			},
-			charset: "utf-8",
-			viewport: "width=device-width, initial-scale=1",
-			title: "Nuxt & Decap CMS boilerplate",
-		},
-	},
-	modules: ["@nuxt/content"],
-	content: {
-		markdown: {
-			// stop the markdownParser from turning headings into anchor tags
-			// Alternatively have a read on markdown render options via the Content module at https://content.nuxt.com/get-started/configuration#markdown
-			anchorLinks: false
-		}
-	},
-	runtimeConfig: {
-		public: {
-			// set your cloudinary env url (used to built responsive assets links)
-			cloudinaryEnvUrl: 'https://res.cloudinary.com/sailsnake-api-test',
-		}
-	},
-	css: [
-		"@/assets/scss/normalize.css",
-		"@/assets/scss/fonts.scss",
-		"@/assets/scss/main.scss",
-		"@/assets/scss/type.scss",
-		"@/assets/scss/forms.scss",
-		"@/assets/scss/cards.scss",
-	],
-	vite: {
-		css: {
-			preprocessorOptions: {
-				scss: {
-					additionalData: '@import "@/assets/scss/mixins.scss"; @import "@/assets/scss/variables.scss";',
-				},
-			},
-		},
-	},
-	experimental: {
-		componentIslands: true,
-	},
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "en",
+      },
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      title: "Nuxt & Decap CMS boilerplate",
+    },
+  },
+
+  modules: ["@nuxt/content"],
+
+  content: {
+    markdown: {
+      // stop the markdownParser from turning headings into anchor tags
+      // Alternatively have a read on markdown render options via the Content module at https://content.nuxt.com/get-started/configuration#markdown
+      anchorLinks: false,
+    },
+  },
+
+  runtimeConfig: {
+    public: {
+      // set your cloudinary env url (used to built responsive assets links)
+      cloudinaryEnvUrl: "https://res.cloudinary.com/sailsnake-api-test",
+    },
+  },
+
+  css: [
+    "@/assets/scss/normalize.css",
+    "@/assets/scss/fonts.scss",
+    "@/assets/scss/main.scss",
+    "@/assets/scss/type.scss",
+    "@/assets/scss/forms.scss",
+    "@/assets/scss/cards.scss",
+  ],
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData:
+            '@import "@/assets/scss/mixins.scss"; @import "@/assets/scss/variables.scss";',
+        },
+      },
+    },
+  },
+
+  experimental: {
+    componentIslands: true,
+  },
+
+  compatibilityDate: "2024-10-09",
 });
